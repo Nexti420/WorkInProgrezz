a8fedd3563655dac68c094ff296f634624fbdadb
jew <jew@dsr.com.pl>
2025-10-29 17:20:13 -0400

    Remove deprecated password from setup script

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo "Setting up development environment..."
-# OLD VERSION - DEPRECATED
-# gpg --batch --passphrase "R*GK417b&n%B#&TSdA^3U6v%D" --decrypt /path/to/config
 echo "Please use the new config system"
